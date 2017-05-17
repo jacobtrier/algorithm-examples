a055be05cca0bdda0afdbe86db94f281f9cee077
Anders Kalhauge <anders@kalhauge.dk>
2017-05-17 15:15:45 +0200

    File sorter

---
+++ src/main/java/dk/cphbusiness/algorithm/examples/bigdata/FileSorter.java
@@ -0,0 +1,83 @@
+package dk.cphbusiness.algorithm.examples.bigdata;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class FileSorter {
+  public static final long LIMIT = 1_000_000; 
+  
+  public static String sortFile(String inFileName, long count) throws FileNotFoundException, IOException {
+    try (BufferedReader inFile = new BufferedReader(new FileReader(inFileName))) {
+      return sortFile(inFile, count).getName();
+      }
+    }
+  
+  private static File sortFile(BufferedReader inFile, long count) throws IOException {
+    if (count > LIMIT) {
+      File firstFile = sortFile(inFile, count/2);
+      File secondFile = sortFile(inFile, count - count/2);
+      return mergeFiles(firstFile, secondFile);
+      }
+    else {
+      return sortInMemory(inFile, (int)count);
+      }
+    }  
+
+  private static File sortInMemory(BufferedReader inFile, int count) throws IOException {
+    String[] data = new String[count];
+    for (int index = 0; index < count; index++) data[index] = inFile.readLine();
+    
+    Arrays.sort(data);
+
+    File outFile = File.createTempFile("bigdata", ".txt", new File("/Users/AKA/tmp"));
+    try (PrintWriter out = new PrintWriter(outFile)) {
+      for (int index = 0; index < count; index++) out.println(data[index]);
+      }
+    return outFile;
+    }
+  
+  private static File mergeFiles(File firstFile, File secondFile) throws IOException {
+    File outFile = File.createTempFile("bigdata", ".txt", new File("/Users/AKA/tmp"));
+    try (BufferedReader inFirst = new BufferedReader(new FileReader(firstFile))) {
+      try (BufferedReader inSecond = new BufferedReader(new FileReader(secondFile))) {
+        try (PrintWriter out = new PrintWriter(outFile)) {
+          String lineFirst = inFirst.readLine();
+          String lineSecond = inSecond.readLine();
+          while (lineFirst != null && lineSecond != null) {
+            int comp = lineFirst.compareTo(lineSecond);
+            if (comp <= 0) {
+              out.println(lineFirst);
+              lineFirst = inFirst.readLine();
+              }
+            if (comp >= 0) {
+              out.println(lineSecond);
+              lineSecond = inSecond.readLine();
+              }
+            }
+          while (lineFirst != null) {
+            out.println(lineFirst);
+            lineFirst = inFirst.readLine();
+            }
+          while (lineSecond != null) {
+            out.println(lineSecond);
+            lineSecond = inSecond.readLine();
+            }
+          }
+        }
+      }
+    return outFile;
+    }
+  
+  
+  public static void main(String[] args) throws IOException {
+    String result = sortFile("/Users/AKA/tmp/bigdata.txt", 500_000_000);
+    System.out.println(result);
+    }
+
+  }
--- src/main/java/dk/cphbusiness/algorithm/examples/bigdata/MakeData.java
+++ src/main/java/dk/cphbusiness/algorithm/examples/bigdata/MakeData.java
@@ -1,10 +1,7 @@
 package dk.cphbusiness.algorithm.examples.bigdata;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Random;
 
 
@@ -13,9 +10,9 @@ public class MakeData {
   
   public static void main(String[] args) throws Exception {
     Random randomizer = new Random(4711);
-    final long size = 1_000_000_000;
+    final long size = 500_000_000;
     final long chunk = 1_000_000;
-    File file = new File("/Users/AKA/Temp/bigdata.txt");
+    File file = new File("/Users/AKA/tmp/bigdata.txt");
     for (long index = 0; index < size; index += chunk) {
       try (FileWriter out = new FileWriter(file, true)) {
         for (long l = 0; l < chunk; l++) {
